8b6ad0558b548c02e11b063b1df8c7a6f9942dec
jwren0 <jwren0@protonmail.com>
2023-06-23 16:45:46 +0100

    Added C

---
+++ c/src/main.h
@@ -0,0 +1,14 @@
+#ifndef EXAMPLE_SRC_MAIN_H
+#define EXAMPLE_SRC_MAIN_H
+
+#include <stdio.h>
+
+/**
+ * Simply the main function.
+ *
+ * @param argc The number of command line arguments.
+ * @param argv The command line arguments.
+ */
+int main(int argc, char *argv[]);
+
+#endif // EXAMPLE_SRC_MAIN_H
